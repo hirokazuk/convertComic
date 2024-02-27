874290aff08de052f5690290f017691011c95d89
hirokazuk <hirokazukamiya@gmail.com>
2024-02-28 03:51:10 +0900

    fix script

--- bin/convertComicJXL.sh
+++ bin/convertComicJXL.sh
@@ -6,12 +6,13 @@ cd "$(dirname "$(readlink -f "$0")")" || exit
 
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/opt/local/bin:/opt/local/sbin:$PATH
 
+SCRIPT_DIR_PATH="$(pwd)"
 
 UUIDSUM=$$ #$(uuidgen | cksum |cut -f 1 -d " ")
-COPIED_ORIGINAL_DIRPATH=./original
+COPIED_ORIGINAL_DIRPATH=${SCRIPT_DIR_PATH}/original
 mkdir -p "${COPIED_ORIGINAL_DIRPATH}"
-EXTRACTED_DIRPATH=./extracted$UUIDSUM
-CONVERTED_DIRPATH=./converted
+EXTRACTED_DIRPATH=${SCRIPT_DIR_PATH}/extracted$UUIDSUM
+CONVERTED_DIRPATH=${SCRIPT_DIR_PATH}/converted
 mkdir -p "${CONVERTED_DIRPATH}"
 FILECOUNT=$#
 FILENAMES=
