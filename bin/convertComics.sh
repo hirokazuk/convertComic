b5b98d379bf0d70f5a46c26c3c327b75768ac1bb
hirokazuk <hirokazukamiya@gmail.com>
2024-02-28 03:05:05 +0900

    update

--- bin/convertComics.sh
+++ bin/convertComics.sh
@@ -18,15 +18,6 @@ exec 2> >(ts|tee -a ./logs/convertComics.log)
 # done
 
 
-UUIDSUM=$$ #$(uuidgen | cksum |cut -f 1 -d " ")
-COPIED_ORIGINAL_DIRPATH=./original
-mkdir ${COPIED_ORIGINAL_DIRPATH}
-EXTRACTED_DIRPATH=./extracted$UUIDSUM
-ZIPED_DIRPATH=./ziped
-mkdir ${ZIPED_DIRPATH}
-FILECOUNT=$#
-FILENAMES=
-
 IFS_BACKUP=$IFS
 IFS=$'\n'
 
@@ -34,21 +25,19 @@ IFS=$'\n'
 dirpaths=()
 for f in "$@"
 do
-  if [[ -f "$f" ]]; then
+  # if [[ -f "$f" ]]; then
 	dirpaths+=( $(dirname "$f") )
-  fi
+  # fi
 done
-# echo @dirpaths@:${dirpaths[@]}
+
 uniq_dirpaths=$( printf "%s\n" "${dirpaths[@]}" | sort -u )
 for uniq_dirpath in ${uniq_dirpaths[@]}
 do
-  if [[ -d "$uniq_dirpath" ]]; then
-	#echo "@uniq_dirpath@:$uniq_dirpath"
+  # if [[ -d "$uniq_dirpath" ]]; then
+	  #echo "@uniq_dirpath@:$uniq_dirpath"
     BACKUP_TIMESTAMP "$uniq_dirpath"
-  fi
+  # fi
 done
-# echo @uniq_dirpaths@:${uniq_dirpaths[@]}
-
 
 pueued -d
 pueue group add  cc
@@ -56,10 +45,13 @@ pueue parallel -g cc 4
 for f in "$@"
 do
   if [[ -f "$f" ]]; then
-	TARGET_FILE_NAME=`basename "${f}"`
-	TARGET_DIR_PATH=`dirname "${f}"`
-	TARGET_DIR_NAME=`basename "${TARGET_DIR_PATH}"`
-	pueue add -g cc -l "${TARGET_DIR_NAME}:${TARGET_FILE_NAME}" -- "convertComicInParallel.sh '${f}'"
+  	TARGET_FILE_NAME=`basename "${f}"`
+  	TARGET_DIR_PATH=`dirname "${f}"`
+  	TARGET_DIR_NAME=`basename "${TARGET_DIR_PATH}"`
+
+    tag --remove $(tag --list -N "$f"| tr ',' '\n' | grep '^CC_'| tr '\n' ',') "$f"
+    tag --add CC_QUEUED "$f"
+  	pueue add -g cc -l "${TARGET_DIR_NAME}:${TARGET_FILE_NAME}" -- "convertComicJXL.sh \"${f}\""
   fi
 done
 
